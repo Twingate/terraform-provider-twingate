d164829ee6d2aeccb5de4398623fdd229fab4106
Lior Rozner <1411811+liorr@users.noreply.github.com>
2021-04-20 21:12:08 -0700

    TG-2603 | Fix lint flows (#5)
    * Fix path for golangci-lint and gosec
    Separate format check into its own step
    
    * fix test and coveralls
    
    * add path to goveralls
    
    * fix lint
    
    * fix lint
    
    Co-authored-by: romankor <kournjaev@gmail.com>

--- Makefile
+++ Makefile
@@ -5,6 +5,7 @@ PKG_NAME=twingate
 BINARY=terraform-provider-${PKG_NAME}
 VERSION=0.1
 OS_ARCH=darwin_amd64
+GOBINPATH=${GOPATH}/bin
 
 default: build
 
@@ -20,40 +21,34 @@ install: build
 	mkdir -p ~/.terraform.d/plugins/${HOSTNAME}/twingate/${PKG_NAME}/${VERSION}/${OS_ARCH}
 	mv ${BINARY} ~/.terraform.d/plugins/${HOSTNAME}/twingate/${PKG_NAME}/${VERSION}/${OS_ARCH}
 
-test: fmtcheck
+test:
 	./scripts/test.sh
 
-testacc: fmtcheck
+testacc:
 	TF_ACC=1 TF_SCHEMA_PANIC_ON_ERROR=1 ./scripts/test.sh
 
-fmt:
-	@echo "==> Fixing source code with gofmt..."
-	gofmt -w -s ./$(PKG_NAME)
-
-# Currently required by tf-deploy compile
 fmtcheck:
 	@echo "==> Checking source code against gofmt..."
 	@sh -c $(CURDIR)/scripts/gofmtcheck.sh
 
 lint: tools
 	@echo "==> Checking source code against linters..."
-	@golangci-lint run ./$(PKG_NAME)
+	@$(GOBINPATH)/golangci-lint run -c golangci.yml ./$(PKG_NAME)
 
 sec: tools
 	@echo "==> Checking source code against security issues..."
-	@gosec ./$(PKG_NAME)
+	@$(GOBINPATH)/gosec ./$(PKG_NAME)
 
 docs: tools
 	tfplugindocs
 
 tools:
-	@echo "==> installing required toolilintng..."
-	go install
-	go install github.com/client9/misspell/cmd/misspell@v0.3.4
-	go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.39.0
-	go install gotest.tools/gotestsum@v1.6.3
-	go install github.com/securego/gosec/v2/cmd/gosec@v2.7.0
-	go install github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs@v0.4.0
+	@echo "==> installing required tools ..."
+	go install github.com/client9/misspell/cmd/misspell@latest
+	go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
+	go install gotest.tools/gotestsum@latest
+	go install github.com/securego/gosec/v2/cmd/gosec@latest
+	go install github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs@latest
 
 test-compile:
 	@if [ "$(TEST)" = "./..." ]; then \
--- twingate/client.go
+++ twingate/client.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -21,24 +22,32 @@ type HTTPClient interface {
 	Do(req *http.Request) (*http.Response, error)
 }
 
+var ErrAPIRequest = errors.New("api request error")
+
+func APIError(op string) error {
+	return fmt.Errorf("APIRequestError %w : %s", ErrAPIRequest, op)
+}
+
 type Client struct {
-	ApiToken     string
+	APIToken     string
 	ServerURL    string
-	ApiServerURL string
+	APIServerURL string
 	HTTPClient   HTTPClient
 }
 
 func NewClient(network, apiToken, url *string) *Client {
-	serverUrl := fmt.Sprintf("https://%s.%s", *network, *url)
+	serverURL := fmt.Sprintf("https://%s.%s", *network, *url)
 	client := Client{
 		HTTPClient:   &http.Client{Timeout: TimeOut},
-		ServerURL:    serverUrl,
-		ApiServerURL: fmt.Sprintf("%s/api/graphql/", serverUrl),
-		ApiToken:     *apiToken,
+		ServerURL:    serverURL,
+		APIServerURL: fmt.Sprintf("%s/api/graphql/", serverURL),
+		APIToken:     *apiToken,
 	}
 	log.Printf("[INFO] Using Server URL %s", client.ServerURL)
+
 	return &client
 }
+
 func (client *Client) ping() error {
 	jsonData := map[string]string{
 		"query": `
@@ -60,7 +69,7 @@ func (client *Client) ping() error {
 
 		return err
 	}
-	log.Printf("[INFO] Graphql API Server at URL %s reachable", client.ApiServerURL)
+	log.Printf("[INFO] Graphql API Server at URL %s reachable", client.APIServerURL)
 
 	return nil
 }
@@ -71,31 +80,31 @@ func Check(f func() error) {
 }
 
 func (client *Client) doRequest(req *http.Request) ([]byte, error) {
-	req.Header.Set("X-API-KEY", client.ApiToken)
+	req.Header.Set("X-API-KEY", client.APIToken)
 	req.Header.Set("content-type", "application/json")
 	res, err := client.HTTPClient.Do(req)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("cant execute http request : %w", err)
 	}
 	defer Check(res.Body.Close)
 	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("cant read response body : %w", err)
 	}
 
 	if res.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("status: %d, body: %s ", res.StatusCode, body)
+		return nil, APIError(fmt.Sprintf("request %s failed, status %d, body %s", req.RequestURI, res.StatusCode, body))
 	}
 
-	return body, err
+	return body, nil
 }
 
 func (client *Client) doGraphqlRequest(query map[string]string) (*gabs.Container, error) {
 	jsonValue, _ := json.Marshal(query)
 
-	req, err := http.NewRequestWithContext(context.Background(), "POST", client.ApiServerURL, bytes.NewBuffer(jsonValue))
+	req, err := http.NewRequestWithContext(context.Background(), "POST", client.APIServerURL, bytes.NewBuffer(jsonValue))
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("could not create request context : %w", err)
 	}
 
 	body, err := client.doRequest(req)
@@ -103,14 +112,14 @@ func (client *Client) doGraphqlRequest(query map[string]string) (*gabs.Container
 	if err != nil {
 		log.Printf("[ERROR] Cant execute request %s", err)
 
-		return nil, err
+		return nil, fmt.Errorf("could not execute request : %w", err)
 	}
 	parsedResponse, err := gabs.ParseJSON(body)
 	if err != nil {
 		log.Printf("[ERROR] Error parsing response %s", string(body))
 
-		return nil, err
+		return nil, fmt.Errorf("could not parse request body : %w", err)
 	}
 
-	return parsedResponse, err
+	return parsedResponse, nil
 }
