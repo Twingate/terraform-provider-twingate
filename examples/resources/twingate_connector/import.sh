a0da2d8186d7b886f2fee634956682e335436f02
romankor <kournjaev@gmail.com>
2021-05-11 15:39:48 -0700

    adding docs

---
+++ examples/resources/twingate_connector/import.sh
@@ -0,0 +1,1 @@
+terraform import twingate_connector.connector Q29ubmVjdG1b0qe0
---
+++ examples/resources/twingate_connector/resource.tf
@@ -0,0 +1,17 @@
+provider "twingate" {
+  api_token = "1234567890abcdef"
+  network   = "mynetwork"
+}
+
+variable "twingate_network" {
+  description = "Name of twingate remote network"
+  default     = "my_test_network"
+}
+
+resource "twingate_remote_network" "network" {
+  name = var.twingate_network
+}
+
+resource "twingate_connector" "connector" {
+  remote_network_id = twingate_remote_network.network.id
+}
