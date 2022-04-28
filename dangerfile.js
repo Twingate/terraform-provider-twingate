ab217e032cf07f1654a4b5a0b8b686a2e9ab2cda
Eran Kampf <eran@ekampf.com>
2022-04-28 11:50:08 -0700

    Fix condition

--- dangerfile.js
+++ dangerfile.js
@@ -5,11 +5,11 @@ const body = danger.github.pr.body.trim()
 const isUser = danger.github.pr.user.type === "User"
 
 if (isUser) {
-  if (!body.includes("Please fill out as much as you can")) {
+  if (body.includes("Please fill out as much as you can")) {
     fail(`Please include meaningful description.`);
   }
 
-  if (!body.includes("Make sure to document important changes")) {
+  if (body.includes("Make sure to document important changes")) {
     fail(`Please include meaningful description.`);
   }
 }
