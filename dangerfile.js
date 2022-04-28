0f1b9920147b85f6f458454a9d80bf5a430c7af5
Eran Kampf <205185+ekampf@users.noreply.github.com>
2022-04-28 11:03:59 -0700

    Relax danger file PR validation rules

--- dangerfile.js
+++ dangerfile.js
@@ -1,25 +1,21 @@
 const {danger, warn, fail} = require('danger')
 
-const regexTitle = /(?<ticket>TG-\d\d\d\d) | .+/
-
 const title = danger.github.pr.title.trim()
-const body = danger.github.pr.body
+const body = danger.github.pr.body.trim()
 const isUser = danger.github.pr.user.type === "User"
 
 if (isUser) {
-  // PR Title should match regexp
-  if (!regexTitle.test(title)) {
-    fail(`Please use standard PR title. (example: "TG-1234 | Some description")`)
-  }
-
   // PR body has to contain the ticket from title
   const match = regexTitle.exec(title);
   if (match === null) {
     process.exit()
   }
 
-  const ticket = match.groups.ticket;
-  if (!body.includes(ticket)) {
-    fail(`Please include the ticket (${ticket}) link in the PR body.`);
+  if (!body.includes("Please fill out as much as you can")) {
+    fail(`Please include meaningful description.`);
+  }
+
+  if (!body.includes("Make sure to document important changes")) {
+    fail(`Please include meaningful description.`);
   }
 }
