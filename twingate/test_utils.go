9dfe4fcdc63c4775f40fd5fcd0834f1014338ba2
Eran Kampf <eran@ekampf.com>
2022-10-24 15:22:36 -0700

    Randomness bad!

--- twingate/test_utils.go
+++ twingate/test_utils.go
@@ -4,15 +4,13 @@ import (
 	"fmt"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
-	"math/rand"
 	"time"
 )
 
 func WaitTestFunc() resource.TestCheckFunc {
 	return func(s *terraform.State) error {
-		// Sleep between 500ms-1s
-		n := 500 + rand.Intn(500) // n will be between 0 and 10
-		time.Sleep(time.Duration(n) * time.Millisecond)
+		// Sleep 500 ms
+		time.Sleep(time.Duration(500) * time.Millisecond)
 		return nil
 	}
 }
