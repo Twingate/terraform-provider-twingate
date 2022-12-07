7ff631b7ccef56adff4095de71d0fd46581af7fc
Volodymyr Manilo <wmanilo@gmail.com>
2022-12-07 11:00:25 +0100

    updated docs

--- examples/resources/twingate_service_account_key/resource.tf
+++ examples/resources/twingate_service_account_key/resource.tf
@@ -9,5 +9,5 @@ resource "twingate_service_account" "github_actions_prod" {
 
 resource "twingate_service_account_key" "github_key" {
   name = "Github Actions PROD key"
-  service_id = twingate_service_account.github_actions_prod.id
+  service_account_id = twingate_service_account.github_actions_prod.id
 }
