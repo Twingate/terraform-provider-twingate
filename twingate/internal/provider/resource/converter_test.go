de31fa09ffc8da9b4b513317628b915617b78d61
Volodymyr Manilo <wmanilo@gmail.com>
2023-01-12 14:11:25 +0100

    added test coverage

--- twingate/internal/provider/resource/converter_test.go
+++ twingate/internal/provider/resource/converter_test.go
@@ -60,3 +60,102 @@ func TestConvertProtocol(t *testing.T) {
 	}
 
 }
+
+func TestConvertPortsRangeToMap(t *testing.T) {
+	cases := []struct {
+		portsRange []*model.PortRange
+		expected   map[int32]struct{}
+	}{
+		{
+			portsRange: nil,
+			expected:   map[int32]struct{}{},
+		},
+		{
+			portsRange: []*model.PortRange{
+				{
+					Start: 70,
+					End:   70,
+				},
+				{
+					Start: 81,
+					End:   85,
+				},
+			},
+			expected: map[int32]struct{}{
+				70: {},
+				81: {},
+				82: {},
+				83: {},
+				84: {},
+				85: {},
+			},
+		},
+		{
+			portsRange: []*model.PortRange{
+				{
+					Start: 80,
+					End:   83,
+				},
+				{
+					Start: 81,
+					End:   85,
+				},
+				{
+					Start: 81,
+					End:   82,
+				},
+			},
+			expected: map[int32]struct{}{
+				80: {},
+				81: {},
+				82: {},
+				83: {},
+				84: {},
+				85: {},
+			},
+		},
+	}
+
+	for n, c := range cases {
+		t.Run(fmt.Sprintf("case_%d", n), func(t *testing.T) {
+			actual := convertPortsRangeToMap(c.portsRange)
+			assert.Equal(t, c.expected, actual)
+		})
+	}
+}
+
+func TestEqualPorts(t *testing.T) {
+	cases := []struct {
+		inputA   []interface{}
+		inputB   []interface{}
+		expected bool
+	}{
+		{
+			inputA:   []interface{}{""},
+			inputB:   []interface{}{""},
+			expected: false,
+		},
+		{
+			inputA:   []interface{}{"80"},
+			inputB:   []interface{}{"80"},
+			expected: true,
+		},
+		{
+			inputA:   []interface{}{"80-81"},
+			inputB:   []interface{}{"80", "81"},
+			expected: true,
+		},
+		{
+			inputA:   []interface{}{"80-81", "70"},
+			inputB:   []interface{}{"70", "80", "81"},
+			expected: true,
+		},
+	}
+
+	for n, c := range cases {
+		t.Run(fmt.Sprintf("case_%d", n), func(t *testing.T) {
+			actual := equalPorts(c.inputA, c.inputB)
+			assert.Equal(t, c.expected, actual)
+		})
+	}
+}
