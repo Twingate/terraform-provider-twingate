4eabb8438019e59853d41f4eb1e13411a1fb9946
Volodymyr Manilo <wmanilo@gmail.com>
2023-02-28 21:13:04 +0100

    added test for setDifference

---
+++ twingate/internal/provider/resource/helper_test.go
@@ -0,0 +1,72 @@
+package resource
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSetIntersection(t *testing.T) {
+	cases := []struct {
+		a        []string
+		b        []string
+		expected []string
+	}{
+		{
+			a:        []string{"1", "2", "3"},
+			b:        []string{"0", "2", "1", "5"},
+			expected: []string{"1", "2"},
+		},
+		{
+			a:        []string{"0", "2", "1", "5"},
+			b:        []string{"1", "2", "3"},
+			expected: []string{"1", "2"},
+		},
+		{
+			a:        []string{"0", "2", "1", "5", "2"},
+			b:        []string{"1", "2", "3"},
+			expected: []string{"1", "2"},
+		},
+	}
+
+	for n, c := range cases {
+		t.Run(fmt.Sprintf("case_%d", n), func(t *testing.T) {
+			actual := setIntersection(c.a, c.b)
+
+			assert.ElementsMatch(t, c.expected, actual)
+		})
+	}
+}
+
+func TestSetDifference(t *testing.T) {
+	cases := []struct {
+		a        []string
+		b        []string
+		expected []string
+	}{
+		{
+			a:        []string{"1", "2", "3"},
+			b:        []string{"0", "2"},
+			expected: []string{"1", "3"},
+		},
+		{
+			a:        []string{"0", "2", "1", "5"},
+			b:        []string{"1", "2", "3"},
+			expected: []string{"0", "5"},
+		},
+		{
+			a:        []string{"1"},
+			b:        []string{"2"},
+			expected: []string{"1"},
+		},
+	}
+
+	for n, c := range cases {
+		t.Run(fmt.Sprintf("case_%d", n), func(t *testing.T) {
+			actual := setDifference(c.a, c.b)
+
+			assert.ElementsMatch(t, c.expected, actual)
+		})
+	}
+}
