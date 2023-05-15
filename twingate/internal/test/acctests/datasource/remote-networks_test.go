b3337196eeec5d5fd31ac99fd2afefc9a42024b9
Eran Kampf <eran@ekampf.com>
2023-05-15 11:05:38 -0700

    inc remote network limit too

--- twingate/internal/test/acctests/datasource/remote-networks_test.go
+++ twingate/internal/test/acctests/datasource/remote-networks_test.go
@@ -36,7 +36,7 @@ func testDatasourceTwingateRemoteNetworks() string {
 
 func TestAccDatasourceTwingateRemoteNetworks_read(t *testing.T) {
 	t.Run("Test Twingate Datasource : Acc Remote Networks Read", func(t *testing.T) {
-		acctests.SetPageLimit(1)
+		acctests.SetPageLimit(10)
 
 		prefix := acctest.RandString(10)
 		networkName1 := test.RandomName(prefix)
