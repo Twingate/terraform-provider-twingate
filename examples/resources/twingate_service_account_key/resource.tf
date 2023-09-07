82ed58028abd94aa64d5abea2513516ddf3e5ec1
Eran Kampf <eran@ekampf.com>
2023-09-07 13:50:28 -0700

    fixed

--- examples/resources/twingate_service_account_key/resource.tf
+++ examples/resources/twingate_service_account_key/resource.tf
@@ -12,14 +12,15 @@ resource "twingate_service_account_key" "github_key" {
   service_account_id = twingate_service_account.github_actions_prod.id
 }
 
-**Key rotation using the [time](https://registry.terraform.io/providers/hashicorp/time/latest) provider**
+
+// Key rotation using the [time](https://registry.terraform.io/providers/hashicorp/time/latest) provider**
 
 resource "time_rotating" "key_rotation" {
   rotation_days = 30
 }
 
-resource "twingate_service_account_key" "github_key" {
-  name = "Github Actions PROD key"
+resource "twingate_service_account_key" "github_key_with_rotation" {
+  name = "Github Actions PROD key (automatically rotating)"
   service_account_id = twingate_service_account.github_actions_prod.id
   lifecycle {
     replace_triggered_by = [time_rotating.key_rotation.rfc3339]
