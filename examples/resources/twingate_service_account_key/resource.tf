5fcdb8701191edd36d3e0b436a0be7d9e80384b7
Eran Kampf <eran@ekampf.com>
2023-09-12 12:19:18 -0700

    Update docs

--- examples/resources/twingate_service_account_key/resource.tf
+++ examples/resources/twingate_service_account_key/resource.tf
@@ -19,10 +19,14 @@ resource "time_rotating" "key_rotation" {
   rotation_days = 30
 }
 
+resource "time_static" "key_rotation" {
+  rfc3339 = time_rotating.key_rotation.rfc3339
+}
+
 resource "twingate_service_account_key" "github_key_with_rotation" {
   name = "Github Actions PROD key (automatically rotating)"
   service_account_id = twingate_service_account.github_actions_prod.id
   lifecycle {
-    replace_triggered_by = [time_rotating.key_rotation.rfc3339]
+    replace_triggered_by = [time_static.key_rotation]
   }
 }
