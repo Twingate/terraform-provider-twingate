c441a0be2473beb7be42de41a8598318596cfbc4
Volodymyr Manilo <35466116+vmanilo@users.noreply.github.com>
2023-09-18 22:08:43 +0300

    Fix: revert resource Resource (#399)
    * revert resource to prevent breaking changes
    
    * doc update
    
    * revert unit tests
    
    * revert resource acctests
    
    * revert ci changes

--- main.go
+++ main.go
@@ -6,14 +6,18 @@ import (
 	"log"
 
 	"github.com/Twingate/terraform-provider-twingate/twingate"
+	twingateV2 "github.com/Twingate/terraform-provider-twingate/twingate/v2"
+	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
+	"github.com/hashicorp/terraform-plugin-go/tfprotov6/tf6server"
+
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
+	"github.com/hashicorp/terraform-plugin-mux/tf5to6server"
+	"github.com/hashicorp/terraform-plugin-mux/tf6muxserver"
 )
 
 var (
 	version = "dev"
 )
-
-const registry = "registry.terraform.io/Twingate/twingate"
 
 func main() {
 	var debug bool
@@ -21,12 +25,33 @@ func main() {
 	flag.BoolVar(&debug, "debug", false, "set to true to run the provider with support for debuggers")
 	flag.Parse()
 
-	err := providerserver.Serve(context.Background(), twingate.New(version),
-		providerserver.ServeOpts{
-			Debug:           debug,
-			Address:         registry,
-			ProtocolVersion: 6,
+	ctx := context.Background()
+	upgradedSdkProvider, err := tf5to6server.UpgradeServer(ctx, twingate.Provider(version).GRPCProvider)
+	if err != nil {
+		log.Fatal(err)
+	}
+	providers := []func() tfprotov6.ProviderServer{
+		func() tfprotov6.ProviderServer {
+			return upgradedSdkProvider
 		},
+		providerserver.NewProtocol6(twingateV2.New(version)()),
+	}
+
+	muxServer, err := tf6muxserver.NewMuxServer(ctx, providers...)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var serveOpts []tf6server.ServeOpt
+	if debug {
+		serveOpts = append(serveOpts, tf6server.WithManagedDebug())
+	}
+
+	err = tf6server.Serve(
+		"registry.terraform.io/Twingate/twingate",
+		muxServer.ProviderServer,
+		serveOpts...,
 	)
 
 	if err != nil {
