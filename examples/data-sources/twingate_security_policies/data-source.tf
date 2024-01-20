9b49c98130d158063e9b4d9578e4af605c37b89a
Volodymyr Manilo <wmanilo@gmail.com>
2024-01-20 07:18:48 +0100

    added filter options for security_policy datasource

--- examples/data-sources/twingate_security_policies/data-source.tf
+++ examples/data-sources/twingate_security_policies/data-source.tf
@@ -1,1 +1,8 @@
-data "twingate_security_policies" "all" {}
+data "twingate_security_policies" "all" {
+  name = "<your security policy's name>"
+  #  name_regexp = "<regular expression of security policy name>"
+  #  name_contains = "<a string in the security policy name>"
+  #  name_exclude = "<your security policy's name to exclude>"
+  #  name_prefix = "<prefix of security policy name>"
+  #  name_suffix = "<suffix of security policy name>"
+}
