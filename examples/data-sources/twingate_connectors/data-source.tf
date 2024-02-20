4e98b1fcecc445194841a809bd4e7e2d45e9064d
Volodymyr Manilo <35466116+vmanilo@users.noreply.github.com>
2024-02-20 21:59:12 +0100

    [After #443] Feature: add support filtering twingate_connectors datasource (#463)
    * added new attributes for resources datasource
    
    * added optional name attributes for resources datasource
    
    * added feature branch
    
    * fix test
    
    * remove feature branch
    
    * enable tests
    
    * remove feature branch
    
    * refactore
    
    * updated docs
    
    * update resources datasource: allow to list all resources
    
    * fix docs
    
    * added optional filters for connectors
    
    * enable tests
    
    * fix test
    
    * revert ci.yml
    
    * fix docs
    
    * fix linter
    
    ---------
    
    Co-authored-by: bertekintw <101608051+bertekintw@users.noreply.github.com>

--- examples/data-sources/twingate_connectors/data-source.tf
+++ examples/data-sources/twingate_connectors/data-source.tf
@@ -1,1 +1,8 @@
-data "twingate_connectors" "all" {}
+data "twingate_connectors" "all" {
+  name = "<your connector's name>"
+  #  name_regexp = "<regular expression of connector name>"
+  #  name_contains = "<a string in the connector name>"
+  #  name_exclude = "<your connector's name to exclude>"
+  #  name_prefix = "<prefix of connector name>"
+  #  name_suffix = "<suffix of connector name>"
+}
