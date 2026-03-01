5737182cee5ee91c99060813f93eb52e2d6b8d1b
Volodymyr Manilo <wmanilo@gmail.com>
2026-03-01 20:53:40 +0100

    fmt

--- twingate/internal/provider/datasource/all-datasources.go
+++ twingate/internal/provider/datasource/all-datasources.go
@@ -1,19 +1,19 @@
 package datasource
 
 const (
-	TwingateGroup               = "twingate_group"
-	TwingateGroups              = "twingate_groups"
-	TwingateRemoteNetwork       = "twingate_remote_network"
-	TwingateRemoteNetworks      = "twingate_remote_networks"
-	TwingateUser                = "twingate_user"
-	TwingateUsers               = "twingate_users"
-	TwingateConnector           = "twingate_connector"
-	TwingateConnectors          = "twingate_connectors"
-	TwingateResource            = "twingate_resource"
-	TwingateResources           = "twingate_resources"
-	TwingateServiceAccounts     = "twingate_service_accounts"
-	TwingateSecurityPolicy      = "twingate_security_policy" // #nosec G101
-	TwingateSecurityPolicies    = "twingate_security_policies"
+	TwingateGroup                    = "twingate_group"
+	TwingateGroups                   = "twingate_groups"
+	TwingateRemoteNetwork            = "twingate_remote_network"
+	TwingateRemoteNetworks           = "twingate_remote_networks"
+	TwingateUser                     = "twingate_user"
+	TwingateUsers                    = "twingate_users"
+	TwingateConnector                = "twingate_connector"
+	TwingateConnectors               = "twingate_connectors"
+	TwingateResource                 = "twingate_resource"
+	TwingateResources                = "twingate_resources"
+	TwingateServiceAccounts          = "twingate_service_accounts"
+	TwingateSecurityPolicy           = "twingate_security_policy" // #nosec G101
+	TwingateSecurityPolicies         = "twingate_security_policies"
 	TwingateDNSFilteringProfile      = "twingate_dns_filtering_profile"
 	TwingateX509CertificateAuthority = "twingate_x509_certificate_authority"
 
