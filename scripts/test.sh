5307dcf8e8e07cb4e421df34e7c14891843922f4
Volodymyr Manilo <wmanilo@gmail.com>
2026-04-17 17:15:04 +0200

    fix merge test coverage results

--- scripts/test.sh
+++ scripts/test.sh
@@ -7,21 +7,30 @@ TEST_RESULTS=${TEST_RESULTS:-"./test_results"}
 
 mkdir -p "${TEST_RESULTS}"
 
+MAIN_COVDIR="${TEST_RESULTS}/covdata-main"
+RETRY_COVDIR="${TEST_RESULTS}/covdata-retry"
+MERGED_COVDIR="${TEST_RESULTS}/covdata-merged"
+rm -rf "${MAIN_COVDIR}" "${RETRY_COVDIR}" "${MERGED_COVDIR}"
+mkdir -p "${MAIN_COVDIR}" "${RETRY_COVDIR}" "${MERGED_COVDIR}"
+
 echo PACKAGE_NAME: "$PACKAGE_NAME"
 echo "Running tests:"
-go tool gotest.tools/gotestsum --packages "${PACKAGE_NAME}" --jsonfile "${TEST_RESULTS}"/report.json -- -coverpkg="${PACKAGE_NAME}" -coverprofile="${TEST_RESULTS}"/coverage.out "${PACKAGE_NAME}"
+go tool gotest.tools/gotestsum --packages "${PACKAGE_NAME}" --jsonfile "${TEST_RESULTS}"/report.json \
+  -- -cover -coverpkg="${PACKAGE_NAME}" "${PACKAGE_NAME}" -args -test.gocoverdir="${MAIN_COVDIR}"
 
 if [ $? -ne 0 ]; then
   set -o errexit
 
   echo "Retry failed tests:"
   grep '"Action":"fail"' "${TEST_RESULTS}"/report.json | grep -o '"Test":"[^"]*"' | awk -F':' '{print $2}' | tr -d '"' > "${TEST_RESULTS}"/failed_tests.txt
-  go tool gotest.tools/gotestsum --rerun-fails=5 --packages "${PACKAGE_NAME}" -- -run "$(paste -sd "|" "${TEST_RESULTS}"/failed_tests.txt)" -coverpkg="${PACKAGE_NAME}" -coverprofile="${TEST_RESULTS}"/retry_coverage.out "${PACKAGE_NAME}"
+  go tool gotest.tools/gotestsum --rerun-fails=5 --packages "${PACKAGE_NAME}" \
+    -- -run "$(paste -sd "|" "${TEST_RESULTS}"/failed_tests.txt)" -cover -coverpkg="${PACKAGE_NAME}" "${PACKAGE_NAME}" \
+       -args -test.gocoverdir="${RETRY_COVDIR}"
 
-  go tool github.com/wadey/gocovmerge "${TEST_RESULTS}"/coverage.out "${TEST_RESULTS}"/retry_coverage.out > "${TEST_RESULTS}"/final_coverage.out
-
+  go tool covdata merge -i="${MAIN_COVDIR},${RETRY_COVDIR}" -o="${MERGED_COVDIR}"
+  go tool covdata textfmt -i="${MERGED_COVDIR}" -o="${TEST_RESULTS}"/final_coverage.out
 else
 
-  go tool github.com/wadey/gocovmerge "${TEST_RESULTS}"/coverage.out "${TEST_RESULTS}"/coverage.out > "${TEST_RESULTS}"/final_coverage.out || exit 1
+  go tool covdata textfmt -i="${MAIN_COVDIR}" -o="${TEST_RESULTS}"/final_coverage.out
 
 fi
